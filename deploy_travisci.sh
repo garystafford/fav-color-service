c556ffcdffe57e825ac097bcfa3894361dbc5787
Gary Stafford <garystafford@rochester.rr.com>
2016-11-23 23:56:05 -0500

    Fixing encrypted env vars for Travis CI

--- deploy_travisci.sh
+++ deploy_travisci.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 # Deploys JAR build artifact to GitHub (acts as pseudo binary repository)
-# (3) encrypted environment variables in .travis.yml file used here
-# travis encrypt GH_TOKEN=<your_token_hash> --add
+# (3) environment variables are encrypted in .travis.yml file and used here
 # travis encrypt COMMIT_AUTHOR_EMAIL=<your_email_here> --add
-# travis encrypt GH_ARTIFACT_REPO=github.com/<your_repo_path>.git --add
+# travis encrypt GH_TOKEN=<your_token_hash> --add
+# travis encrypt GH_COLOR_ARTIFACT_REPO=github.com/<your_repo_path>.git --add
 
 #set -x
 
