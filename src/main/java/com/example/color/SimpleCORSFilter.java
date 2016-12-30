e400e923e9a35373cc29e5a6d8dbbe529009672e
Gary Stafford <garystafford@rochester.rr.com>
2016-12-29 22:12:49 -0500

    Updating CORS options

--- src/main/java/com/example/color/SimpleCORSFilter.java
+++ src/main/java/com/example/color/SimpleCORSFilter.java
@@ -29,7 +29,7 @@ public class SimpleCORSFilter implements Filter {
         //response.setHeader("Access-Control-Allow-Origin", request.getHeader("Origin"));
         response.setHeader("Access-Control-Allow-Origin", "*");
         response.setHeader("Access-Control-Allow-Credentials", "true");
-        response.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE, OPTIONS, HEAD");
+        response.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE, PATCH, OPTIONS, HEAD");
         response.setHeader("Access-Control-Max-Age", "3600");
         response.setHeader("Access-Control-Allow-Headers", "Content-Type, Accept, X-Requested-With, remember-me");
 
